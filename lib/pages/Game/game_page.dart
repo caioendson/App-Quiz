dbeedd0ed5c804fa5890167a5be24b5724d265a0
caioendson <caioendson.ead@gmail.com>
2020-09-22 03:07:45 -0300

    features on Game

--- lib/pages/Game/game_page.dart
+++ lib/pages/Game/game_page.dart
@@ -11,24 +11,50 @@ class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: PageView.builder(
         physics: NeverScrollableScrollPhysics(),
         itemCount: 4,
         controller: _controllerPageView,
         itemBuilder: (cntx, inx) => Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.only(top: 50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Pergunta $inx'),
-              FlatButton(
-                onPressed: () {
-                  _controllerPageView.nextPage(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.ease);
-                },
-                child: Text('Responder'),
-              )
+              Container(
+                color: Colors.grey[800],
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  ' O que signifca Things na internet das coisas? ' * (inx + 1),
+                  textWidthBasis: TextWidthBasis.parent,
+                  textScaleFactor: 2,
+                  style: TextStyle(
+                    backgroundColor: Colors.grey[800],
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                child: Column(
+                  children: [1, 2, 3]
+                      .map((e) => FlatButton(
+                            color: Colors.white,
+                            onPressed: () {
+                              _controllerPageView.nextPage(
+                                  duration: Duration(milliseconds: 500),
+                                  curve: Curves.ease);
+                            },
+                            child: Row(
+                              children: [
+                                Text('Responder'),
+                              ],
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
             ],
           ),
         ),
